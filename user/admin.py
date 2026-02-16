4321cb0de96b99ab879f39f0ef3a455245dbebbf
gustavo <gusgurtavo@gmail.com>
2026-02-16 14:39:05 -0300

    statisc

--- user/admin.py
+++ user/admin.py
@@ -4,8 +4,8 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'institution', 'research_area', 'email_verified', 'created_at')
+    list_display = ('user', 'institution', 'research_area', 'email_verified', 'created_at', 'email_verified','created_at'
+,'updated_at')
     list_filter = ('email_verified', 'created_at')
     search_fields = ('user__username', 'user__email', 'institution', 'research_area')
-    readonly_fields = ('created_at', 'updated_at')
     date_hierarchy = 'created_at'
